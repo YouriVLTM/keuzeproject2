c60baa5636111184b8adac82cde48fc1977e016b
Luca <r0758311@student.thomasmore.be>
2019-05-20 14:37:05 +0200

    adpark

--- src/java/fact/it/www/dataaccess/DAPark.java
+++ src/java/fact/it/www/dataaccess/DAPark.java
@@ -19,6 +19,56 @@ public class DAPark {
         this.login = login;
         this.password = password;
     }
+    
+    
+     public Park getPark() {
+        Park park = null;
+
+        try (
+                Connection connection = DriverManager.getConnection(url, login, password);
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM park where id = 1");) {
+            if (resultSet.next()) {
+                park = new Park();
+                park.setId(resultSet.getInt("id"));
+                park.setNaam(resultSet.getString("naam"));
+                park.setRegioid(resultSet.getInt("regioid"));
+                park.setAantalSterren(resultSet.getInt("aantalSterren"));
+                park.setVoorzieningen(resultSet.getString("voorzieningen"));
+                park.setFoto(resultSet.getString("foto"));
+                
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return park;
+    }
+    
+    
+    public Park getPark(String naam) {
+        Park park = null;
+
+        try (
+                Connection connection = DriverManager.getConnection(url, login, password);
+                PreparedStatement statement = connection.prepareStatement("SELECT * FROM park where naam like ?");     ) {
+                statement.setString(1, naam + "%");
+                ResultSet resultSet = statement.executeQuery();
+           
+            if (resultSet.next()) {
+                park = new Park();
+                park.setId(resultSet.getInt("id"));
+                park.setNaam(resultSet.getString("naam"));
+                park.setRegioid(resultSet.getInt("regioid"));
+                park.setAantalSterren(resultSet.getInt("aantalSterren"));
+                park.setVoorzieningen(resultSet.getString("voorzieningen"));
+                park.setFoto(resultSet.getString("foto"));
+                
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return park;
+    }
 
    
 }
---
+++ web/park.jsp
@@ -0,0 +1,22 @@
+<%-- 
+    Document   : park
+    Created on : 20-mei-2019, 13:00:35
+    Author     : lukak
+--%>
+
+<%@page import="fact.it.www.beans.Park"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <%Park park = (Park) request.getAttribute("park");%>
+        <h1>VakantiePark</h1>
+        <p>VakantiePark: <%=park.getNaam()%> </p>  
+        <p>AantalSterren: <%=park.getAantalSterren()%> </p> 
+        <p>  <a href="index.jsp">Terug naar beginpagina</a></p>
+    </body>
+</html>
